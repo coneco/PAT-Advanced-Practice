140efc55192c0e7767676d36de7530d0fa4c2bf0
coneco <coneco@outlook.com>
2016-09-26 19:56:07 +0800

    got 1014

--- 1014 Waiting in Line/waiting_in_line.cpp
+++ 1014 Waiting in Line/waiting_in_line.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 #include <queue>
 #define NL -1
+#define EPT -1
+#define CLZ -2
 
 typedef struct node
 {
@@ -76,7 +78,7 @@ public:
 };
 
 int line2insert(Line* lines, int n, int m);
-int allEmpty(Line* lines, int n);
+int canFin(int* tRemain, int n);
 
 int main(int argc, char const *argv[])
 {
@@ -89,9 +91,24 @@ int main(int argc, char const *argv[])
   int* tRemain = new int[n];
   Line* lines = new Line[n];
 
+  std::queue<int> queries;
+
+  int maxT = 0;
   for (int i = 0; i < k; ++i)
   {
     std::scanf("%d", t + i);
+    if (t[i] > maxT)
+    {
+      maxT = t[i];
+    }
+    tFinish[i] = -1;
+  }
+
+  for (int i = 0; i < q; ++i)
+  {
+    int query;
+    std::scanf("%d", &query);
+    queries.push(query - 1);
   }
 
   for (int i = 0; i < n; ++i)
@@ -99,7 +116,7 @@ int main(int argc, char const *argv[])
     tRemain[i] = 0;
   }
 
-  while (behindLine < k && behindLine < n)
+  while (behindLine < k && behindLine < n) //初始化，铺满窗口一层
   {
     tRemain[behindLine] = t[behindLine];
     lines[behindLine].push(behindLine);
@@ -110,18 +127,27 @@ int main(int argc, char const *argv[])
   {
 
     int l2i;
-    while (((l2i = line2insert(lines, n, m)) != NL) && behindLine < k)
+    while (((l2i = line2insert(lines, n, m)) != NL) && behindLine < k && tNow <= 540) //插满队列
     {
       lines[l2i].push(behindLine);
       ++behindLine;
     }
 
-
-    int minRI = 0;
-    int minRemain = tRemain[0];
-    for (int i = 1; i < n; ++i)
+    for (int i = 0; i < n; ++i)
     {
-      if (tRemain[i] < minRemain)
+      if (tRemain[i] == EPT && lines[i].size())
+      {
+        pCustomer cCus = lines[i].top();
+        tRemain[i] = t[cCus->id];
+      }
+    }
+
+
+    int minRI;
+    int minRemain = maxT + 1;
+    for (int i = 0; i < n; ++i) // 时间经过minRemain
+    {
+      if (tRemain[i] != EPT && tRemain[i] != CLZ && tRemain[i] < minRemain)
       {
         minRI = i;
         minRemain = tRemain[i];
@@ -131,16 +157,43 @@ int main(int argc, char const *argv[])
 
     for (int i = 0; i < n; ++i)
     {
-      tRemain[i] -= minRemain;
+      if (tRemain[i] != EPT && tRemain[i] != CLZ)
+      {
+        tRemain[i] -= minRemain;
+      }
     }
 
-    pCustomer cur = lines[minRI].top();
+    pCustomer cur = lines[minRI].top(); //离开队列
     tFinish[cur->id] = tNow;
     lines[minRI].pop();
-    pCustomer next1 = lines[minRI].top();
-    tRemin[minRI] = t[next1->id];
+    if (tNow >= 540)
+    {
+      tRemain[minRI] = CLZ;
+      continue;
+    }
 
-  } while (!allEmpty(lines, n));
+    if (lines[minRI].size())
+    {
+      pCustomer next1 = lines[minRI].top();
+      tRemain[minRI] = t[next1->id];
+    } else {
+      tRemain[minRI] = EPT;
+    }
+    
+
+  } while (!canFin(tRemain, n));
+
+  while(!queries.empty()) 
+  {
+    int timeFin = tFinish[queries.front()];
+    if (timeFin == -1)
+    {
+      std::printf("Sorry\n");
+    } else {
+      std::printf("%.2d:%.2d\n", timeFin / 60 + 8, timeFin % 60);
+    }
+    queries.pop();
+  }
 
   
   delete[] t;
@@ -172,11 +225,11 @@ int line2insert(Line* lines, int n, int m)
   }
 }
 
-int allEmpty(Line* lines, int n)
+int canFin(int* tRemain, int n)
 {
   for (int i = 0; i < n; ++i)
   {
-    if (lines[i].size() > 0)
+    if (tRemain[i] != EPT && tRemain[i] != CLZ)
     {
       return 0;
     }
