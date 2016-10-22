efb303ae0a9e61e02c873fe00226209113588ed1
coneco <coneco@outlook.com>
2016-10-22 11:15:02 +0800

    1032 solved

--- 1032 Sharing/sharing.cpp
+++ 1032 Sharing/sharing.cpp
@@ -1,39 +1,42 @@
 #include <cstdio>
 #include <map>
+#define MAX 100000
 
 int main(int argc, char const *argv[])
 {
-  std::map<int, bool> linked;
   int f1, f2, n;
   std::scanf("%d %d %d", &f1, &f2, &n);
-  if (f1 == f2)
+  
+  int dict[MAX];
+  bool used[MAX] = {false};
+
+  for (int i = 0; i < n; ++i)
   {
-    std::printf("%.5d\n", f1);
-  } 
+    int addr, next;
+    char c;
+    std::scanf("%d %c %d", &addr, &c, &next);
+    dict[addr] = next;
+  }
+
+  while (f1 != -1)
+  {
+    used[f1] = true;
+    f1 = dict[f1];
+  }
+
+  while (!used[f2] && f2 != -1)
+  {
+    f2 = dict[f2];
+  }
+  
+  if (f2 == -1)
+  {
+    std::printf("-1\n");
+  }
   else
   {
-    for (int i = 0; i < n; ++i)
-    {
-      int addr, next;
-      char data;
-      std::scanf("%d %c %d", &addr, &data, &next);
-      if (linked[next])
-      {
-        if (next == -1)
-        {
-          std::printf("%d\n", next);
-        }
-        else
-        {
-          std::printf("%.5d\n", next);
-        }
-        break;
-      }
-      else
-      {
-        linked[next] = true;
-      }
-    }
+    std::printf("%.5d\n", f2);
   }
+  
   return 0;
 }
