b6c013fdb90d78435f433b87fdbdc7bfd67c4c4a
coneco <coneco@outlook.com>
2016-11-26 16:03:15 +0800

    solving 1047

---
+++ 1047 Student List for Course/students.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::vector<std::vector<string> > list(2501);
+
+int main(int argc, char const *argv[])
+{
+  int n, k;
+  
+  return 0;
+}
