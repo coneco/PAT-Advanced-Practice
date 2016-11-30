3274c8f376e8f7bf93f1b5ba591956e740daadf9
coneco <coneco@outlook.com>
2016-11-30 19:21:05 +0800

    solve 1060

--- 1060 Are They Equal/equal.cpp
+++ 1060 Are They Equal/equal.cpp
@@ -1,43 +1,88 @@
 #include <cstdio>
-#include <cmath>
+#include <cstring>
 
-void sci(double d, int* w, double* digit);
+void getd(char* n, int len, char* nn, int* k);
 
 int main(int argc, char const *argv[])
 {
-  int n, nn = 1;
-  double d1, d2;
-  std::scanf("%d %lf %lf", &n, &d1, &d2);
-  for (int i = 0; i < n; ++i)
+  char n1[200];
+  char n2[200];
+  char nn1[200];
+  char nn2[200];
+  int n, len1, len2;
+  std::scanf("%d %s %s", &n, &n1, &n2);
+  
+  getd(n1, n, nn1, &len1);
+  getd(n2, n, nn2, &len2);
+  if (strcmp(nn1, nn2) == 0 && len1 == len2)
   {
-    nn *= 10;
-  }
-  int w1, w2;
-  double dg1, dg2;
-  sci(d1, &w1, &dg1);
-  sci(d2, &w2, &dg2);
-  int dgi1 = floor(dg1 * nn);
-  int dgi2 = floor(dg2 * nn);
-  if (dgi1 == dgi2 && w1 == w2)
-  {
-    printf("YES 0.%d*10^%d\n", dgi1, w1);
+    std::printf("YES 0.%s*10^%d\n", nn1, len1);
   }
   else
   {
-    printf("NO 0.%d*10^%d 0.%d*10^%d\n", dgi1, w1, dgi2, w2);
+    std::printf("NO 0.%s*10^%d 0.%s*10^%d\n", nn1, len1, nn2, len2);
   }
   return 0;
 }
 
-void sci(double d, int* w, double* digit)
+void getd(char* n, int len, char* nn, int* k)
 {
-  int k = 0;
-
-  while(d >= 1) 
+  int i = 0, cnt = 0, pPoint = 0, pBegin;
+  bool begin = false, end = false;
+  while(n[i]) 
   {
-    d /= 10;
-    ++k;
+    if (n[i] == '.')
+    {
+      pPoint = i;
+      ++i;
+      continue;
+    }
+    if (!cnt && n[i] == '0')
+    {
+      ++i;
+      continue;
+    }
+    if (!cnt)
+    {
+      pBegin = i;
+    }
+    if (cnt < len)
+    {
+      nn[cnt] = n[i];
+      ++cnt;
+      ++i;
+    }
+    else
+    {
+      ++i;
+    }
+    
+    if (cnt == len)
+    {
+      end = true;
+    }
   }
-  *w = k;
-  *digit = d;
+
+  bool zero = false;
+  while(cnt < len) 
+  {
+    nn[cnt] = '0';
+    ++cnt;
+  }
+  
+  nn[len] = '\0';
+  
+  if (!pPoint)
+  {
+    pPoint = i;
+  }
+  *k = pPoint - pBegin;
+  if (*k < 0)
+  {
+    ++*k;
+  }
+  if (nn[0] == '0')
+  {
+    *k = 0;
+  }
 }
